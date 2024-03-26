80ad7b23b145cde6ee7b497c72f48d960e2617a4
Sivanesan-P <140958543+Sivanesan-P@users.noreply.github.com>
2024-03-26 19:56:25 +0530

    Parentheses Checker

---
+++ parentheses_checker.py
@@ -0,0 +1,20 @@
+def parentheses_checker(input):
+    for i in input:
+        if i==')' and not list:
+            print("parentheses is missing")
+            return
+        elif i=='(':
+            list.append(i)
+        elif i==")" and len(list)>=1:
+            list.remove('(')
+    if not list:
+        print("parentheses are balanced")  
+    else:
+        print("parentheses is missing")
+
+
+input=input("enter:")
+list=[]
+parentheses_checker(input)
+
+                
